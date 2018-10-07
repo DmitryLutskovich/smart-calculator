05a2e2f2f0bed7e94c5a5f09e56b0d56bfc5dce3
DmitryLutskovich <lutskovich.dima.99@mail.ru>
2018-10-07 21:36:24 +0300

    calculator

--- src/index.js
+++ src/index.js
@@ -1,27 +1,48 @@
-class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+class SmartCalculator{
+	constructor(initialValue) {
+		this.result = [initialValue];
+	}
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+	add(number) {
+		this.result.push("+");
+		this.result.push(number);
+		return this;
+	}
 
-  multiply(number) {
-    // your implementation
-  }
+	subtract(number) {
+		this.result.push("-");
+		this.result.push(number);
+		return this;
+	}
 
-  devide(number) {
-    // your implementation
-  }
+	multiply(number) {
+		this.result.push("*");
+		this.result.push(number);
+		return this;
+	}
 
-  pow(number) {
-    // your implementation
-  }
+	devide(number) {
+		this.result.push("/");
+		this.result.push(number);
+		return this;
+	}
+
+	pow(number) {
+		if(this.result[this.result.length-1][2]===")"){
+			let number1 = this.result[this.result.length-1][1];
+			number=Math.pow(number1,number);
+			this.result[this.result.length-1] = this.result[this.result.length-1].replace(number1, number);
+		}else{
+			this.result.splice(this.result.length-1, 0, "Math.pow(");
+			this.result.push(","+ number + ")");
+		}
+		return this;
+	}
+
+	valueOf() {
+		return eval(this.result.join(" "));
+	}
+
 }
 
 module.exports = SmartCalculator;
